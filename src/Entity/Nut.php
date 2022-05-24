cf52bdc07e38eec2b68e17ec189f1963323868f7
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-05-24 16:46:23 +0200

    all

---
+++ src/Entity/Nut.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NutRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Persistence\ObjectManager;
+
+#[ORM\Entity(repositoryClass: NutRepository::class)]
+class Nut
+{
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 100)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $stock;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+}
